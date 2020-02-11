d299580033d1c5c43d822a2b22e62cb3fd2fc844
Alexander Bigga <alexander.bigga@slub-dresden.de>
2020-02-11 17:22:41 +0100

    Add basic configuration to make first, unstyled output

---
+++ ext_emconf.php
@@ -0,0 +1,23 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'SLUB Web Boersenblatt Digital',
+    'description' => 'Website configuration and templates of Boersenblatt Digital (www.boersenblat-digital.de)',
+    'category' => 'templates',
+    'author' => 'Alexander Bigga',
+    'author_email' => 'typo3@slub-dresden.de',
+    'state' => 'beta',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'clearCacheOnLoad' => 0,
+    'version' => '0.0.1',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '9.5.0-9.5.99',
+        ),
+        'conflicts' => array(
+        ),
+        'suggests' => array(
+        ),
+    ),
+);
